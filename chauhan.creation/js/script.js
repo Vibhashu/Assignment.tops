7f6bd5cb86aa625b449089179c8207d8fd414eea
Vibhashu <vibhanshu.chauhan2006@gmail.com>
2025-04-03 09:55:51 +0530

    Add files via upload

---
+++ chauhan.creation/js/script.js
@@ -0,0 +1,22 @@
+contact1 = document.querySelector(".button1")
+      
+      function contact_1()
+      {
+        contact1.innerHTML = "+91 8141768476"
+      }
+
+      contact2 = document.querySelector(".button2")
+      
+      function contact_2()
+      {
+        contact2.innerHTML = "+91 6352196020"
+      }
+      nav = document.querySelector(".nav1")
+
+      function nav2() {
+        if (nav.style.display == "none") {
+          nav.style.display = "block";
+        } else {
+          nav.style.display = "none";
+        }
+      }
